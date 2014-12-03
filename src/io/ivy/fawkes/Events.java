b09e8a28c34aeb3359e849f0f4c50cd74f7622e5
Clint Moore <clint@ivy.io>
2014-12-03 02:09:56 -0800

    ...

--- src/io/ivy/fawkes/Events.java
+++ src/io/ivy/fawkes/Events.java
@@ -20,13 +20,30 @@ public class Events implements Listener {
     
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-    	if (event.getDamager().getType().equals(EntityType.PLAYER)) {
-    		
-    	}
+
+        String damager_name;
+        String damagee_name;
+
+        if (event.getDamager().getType().equals(EntityType.PLAYER) ||
+            event.getEntity().getType().equals(EntityType.PLAYER)) {
+
+            Double pre_damage = Double.valueOf(event.getDamage());
+            Double end_damage = Double.valueOf(event.getFinalDamage());
+
+            String damager = event.getDamager().getType().toString();
+            String damagee = event.getEntity().getType().toString();
+            
+            Bukkit.getLogger().info(damager + " HIT " + damagee + " for " + String.valueOf(pre_damage) + "/" + String.valueOf(end_damage));
+        }
     }
     
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
+        if (event.getEntity().getKiller() != null) {
+            Entity entity = event.getEntity();
+            Player player = entity.getKiller();
+            Bukkit.getLogger().info(player.getName() + " killed a " + entity.getType().toString + ".");
+        }
     }
     
     @EventHandler
@@ -37,9 +54,13 @@ public class Events implements Listener {
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
     	if (event.getEntityType().equals(EntityType.PLAYER)) {
     		if (event.getFoodLevel() < 10) {
-    			Bukkit.getLogger().info("Adjusted food level.");
     			Player player = (Player) event.getEntity();
-    			player.setFoodLevel(event.getFoodLevel() + 1);
+          int food_level = event.getFoodLevel();
+          int new_food_level = food_level + 2;
+          
+    			player.setFoodLevel(event.getFoodLevel() + 5);
+    			Bukkit.getLogger().info("Adjusted food level from " + String.valueOf(food_level) + " to " + String.valueOf(new_food_level));
+          
     		}
     	}
     }
